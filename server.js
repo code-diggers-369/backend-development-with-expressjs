6ba854170d302308b7d210bbb6c0a87fe32bc9f1
code-diggers-369 <codediggers007@gmail.com>
2022-08-13 14:12:13 +0530

    Multiple Method Types Handle With Expressjs

--- server.js
+++ server.js
@@ -11,3 +11,15 @@ app.listen(PORT, () => {
 app.get("/", (req, res, next) => {
   res.send("Hello World");
 });
+
+app.post("/", (req, res, next) => {
+  res.send("<h1>Hii</h1>");
+});
+
+app.put("/", (req, res, next) => {
+  res.send("this is put method");
+});
+
+app.delete("/", (req, res, next) => {
+  res.send("method = delete");
+});
