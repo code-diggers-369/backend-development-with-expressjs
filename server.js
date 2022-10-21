ad10c5d65d40228338acc437cd93aea1ca66d83c
code-diggers-369 <codediggers007@gmail.com>
2022-10-21 21:58:03 +0530

    4 - 5 CRUD operation in expressjs

--- server.js
+++ server.js
@@ -4,22 +4,72 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+//
+app.use(express.json());
+
 app.listen(PORT, () => {
   console.log("Server Is Running On Port :-" + PORT);
 });
 
-app.get("/", (req, res, next) => {
-  res.send("Hello World");
+//
+var usersData = [
+  { name: "Haresh", rollNo: 21 },
+  { name: "Mohit", rollNo: 22 },
+  { name: "Prashant", rollNo: 23 },
+  { name: "Uvesh", rollNo: 24 },
+  { name: "Yuvraj", rollNo: 25 },
+];
+
+app.get("/user/fetchlist", (req, res) => {
+  return res.json(usersData);
 });
 
-app.post("/", (req, res, next) => {
-  res.send("<h1>Hii</h1>");
+app.post("/user/add", (req, res) => {
+  const { name, rollno } = req.body;
+
+  usersData.push({
+    name: name,
+    rollNo: rollno,
+  });
+
+  return res.send("Data Addedd Successfully");
 });
 
-app.put("/", (req, res, next) => {
-  res.send("this is put method");
+app.put("/user/update", (req, res) => {
+  const { name, rollno } = req.body;
+
+  const tempData = usersData.map((item) => {
+    if (item.rollNo == rollno) {
+      return {
+        ...item,
+        name: name,
+      };
+    } else {
+      return item;
+    }
+  });
+
+  usersData = tempData;
+
+  return res.send("Data Update Successfully");
 });
 
-app.delete("/", (req, res, next) => {
-  res.send("method = delete");
+app.delete("/user/remove", (req, res) => {
+  const { rollno } = req.body;
+
+  const tempData = usersData.filter((item) => {
+    if (item.rollNo != rollno) {
+      return item;
+    }
+  });
+
+  usersData = tempData;
+
+  return res.send("Data Remove Successfully");
 });
+
+// CRUD
+// C => CREATE
+// R => READ
+// U => UPDATE
+// D => DELETE
